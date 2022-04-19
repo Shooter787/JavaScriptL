0aeebea70ee81cece54f65827256010898963b3d
Danil <93434372+Shooter787@users.noreply.github.com>
2022-04-19 06:36:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+class GoodsItem {
+constructor(title, price) {
+this.title = title;
+this.price = price;
+}
+render() {
+return `<div
+class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+}
+}
+class GoodsList {
+constructor() {
+this.goods = [];
+}
+fetchGoods() {
+this.goods = [
+{ title: 'Shirt', price: 150 },
+{ title: 'Socks', price: 50 },
+{ title: 'Jacket', price: 350 },
+{ title: 'Shoes', price: 250 },
+];
+}
+render() {
+let listHtml = '';
+this.goods.forEach(good => {
+const goodItem = new GoodsItem(good.title, good.price);
+listHtml += goodItem.render();
+});
+document.querySelector('.goods-list').innerHTML = listHtml;
+}
+}
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+
+//Подсчет суммы корзины.*//
+renderGoodsList(goods); 
+let goods_price = 0;
+function countGoodsPrice(x) {
+    for (i = 0; i < goods.length; i++) {
+       goods_price += goods[i].price;
+    }
+    return goods_price;
+}
+console.log(countGoodsPrice(goods));
